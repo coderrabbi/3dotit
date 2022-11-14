276e16c5b8acc41fb7ae5eac96de7c7d6cb25eb7
NAYEEM65 <mathnayeem054@gmail.com>
2022-11-14 23:20:59 +0600

    component folder added and demo header added

---
+++ client/components/Layout/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../Common/Navbar/Navbar';
+
+const Layout = () => {
+    return (
+        <>
+            <Navbar />
+        </>
+    );
+};
+
+export default Layout;
