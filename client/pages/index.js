7ffc42bc0f12665d19fedad105dcbf3360ada222
NAYEEM65 <mathnayeem054@gmail.com>
2022-11-16 12:53:57 +0600

    framer motion added

--- client/pages/index.js
+++ client/pages/index.js
@@ -6,16 +6,16 @@ import Loader from '../components/Common/Loader/Loader';
 
 export default function Home() {
     const [loading, setLoading] = useState(false);
-    useEffect(() => {
-        Router.events.on('routeChangeStart', () => setLoading(true));
-        Router.events.on('routeChangeComplete', () => setLoading(false));
-        Router.events.on('routeChangeError', () => setLoading(false));
-        return () => {
-            Router.events.off('routeChangeStart', () => setLoading(true));
-            Router.events.off('routeChangeComplete', () => setLoading(false));
-            Router.events.off('routeChangeError', () => setLoading(false));
-        };
-    }, [Router.events]);
+    // useEffect(() => {
+    //     Router.events.on('routeChangeStart', () => setLoading(true));
+    //     Router.events.on('routeChangeComplete', () => setLoading(false));
+    //     Router.events.on('routeChangeError', () => setLoading(false));
+    //     return () => {
+    //         Router.events.off('routeChangeStart', () => setLoading(true));
+    //         Router.events.off('routeChangeComplete', () => setLoading(false));
+    //         Router.events.off('routeChangeError', () => setLoading(false));
+    //     };
+    // }, [Router.events]);
     return (
         <div>
             <Head>
