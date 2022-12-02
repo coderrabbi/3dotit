a5169f02a49c7bf17f24299e14547d9945ce98cb
coderrabbie <golamrabbi.dv@gmail.com>
2022-12-03 01:53:13 +0600

    lottie added

--- client/components/Hero/Hero.js
+++ client/components/Hero/Hero.js
@@ -1,120 +1,126 @@
-import React from 'react';
-import { useRef } from 'react';
-import Autoplay from 'embla-carousel-autoplay';
-import { Carousel } from '@mantine/carousel';
-import { useMediaQuery } from '@mantine/hooks';
-import { createStyles, Paper, Text, Title, Button, useMantineTheme } from '@mantine/core';
+import React from "react";
+import { useRef } from "react";
+import Autoplay from "embla-carousel-autoplay";
+import { Carousel } from "@mantine/carousel";
+import { useMediaQuery } from "@mantine/hooks";
+import {
+  createStyles,
+  Paper,
+  Text,
+  Title,
+  Button,
+  useMantineTheme,
+} from "@mantine/core";
+import Lottie from "lottie-react";
+import heroImg from "../../public/assets/hero-image.json";
+import heroImg3 from "../../public/assets/3dotit-hero-image3.json";
 const Hero = () => {
-    const autoplay = useRef(Autoplay({ delay: 2000 }));
-    const useStyles = createStyles((theme) => ({
-        card: {
-            height: '85vh',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'space-between',
-            alignItems: 'flex-start',
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-        },
+  const autoplay = useRef(Autoplay({ delay: 2000 }));
+  const useStyles = createStyles((theme) => ({
+    card: {
+      display: "flex",
 
-        title: {
-            fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-            fontWeight: 900,
-            color: theme.white,
-            lineHeight: 1.2,
-            fontSize: 32,
-            marginTop: theme.spacing.xs,
-        },
+      justifyContent: "space-between",
+      alignItems: "flex-start",
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+    },
 
-        category: {
-            color: theme.white,
-            opacity: 0.7,
-            fontWeight: 700,
-            textTransform: 'uppercase',
-        },
-    }));
-    function Card({ image, title, category }) {
-        const { classes } = useStyles();
+    title: {
+      fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+      fontWeight: 900,
+      color: theme.dark,
+      lineHeight: 1.2,
+      fontSize: 32,
+      marginTop: theme.spacing.xs,
+    },
 
-        return (
-            <Paper
-                shadow="md"
-                p="xl"
-                sx={{ backgroundImage: `url(${image})` }}
-                className={`${classes.card} relative`}
-            >
-                <div className="absolute bg-black/40 top-0 left-0 z-10 w-full h-full" />
-                <div className="relative z-20">
-                    <Text className={`${classes.category}`} size="xs">
-                        {category}
-                    </Text>
-                    <Title order={3} className={`${classes.title}`}>
-                        {title}
-                    </Title>
-                    <Button variant="white" color="white">
-                        Read article
-                    </Button>
-                </div>
-            </Paper>
-        );
-    }
-    const data = [
-        {
-            image: 'https://images.unsplash.com/photo-1508193638397-1c4234db14d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
-            title: 'Best forests to visit in North America',
-            category: 'nature',
-        },
-        {
-            image: 'https://images.unsplash.com/photo-1559494007-9f5847c49d94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
-            title: 'Hawaii beaches review: better than you think',
-            category: 'beach',
-        },
-        {
-            image: 'https://images.unsplash.com/photo-1608481337062-4093bf3ed404?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
-            title: 'Mountains at night: 12 best locations to enjoy the view',
-            category: 'nature',
-        },
-        {
-            image: 'https://images.unsplash.com/photo-1507272931001-fc06c17e4f43?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
-            title: 'Aurora in Norway: when to visit for best experience',
-            category: 'nature',
-        },
-        {
-            image: 'https://images.unsplash.com/photo-1510798831971-661eb04b3739?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
-            title: 'Best places to visit this winter',
-            category: 'tourism',
-        },
-        {
-            image: 'https://images.unsplash.com/photo-1582721478779-0ae163c05a60?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
-            title: 'Active volcanos reviews: travel at your own risk',
-            category: 'nature',
-        },
-    ];
-
-    const theme = useMantineTheme();
-    const mobile = useMediaQuery(`(max-width: ${theme.breakpoints.sm}px)`);
-    const slides = data.map((item) => (
-        <Carousel.Slide key={item.title}>
-            <Card {...item} />
-        </Carousel.Slide>
-    ));
+    category: {
+      color: theme.dark,
+      opacity: 0.7,
+      fontWeight: 700,
+      textTransform: "uppercase",
+    },
+  }));
+  function Card({ image, title, category }) {
+    const { classes } = useStyles();
 
     return (
-        <Carousel
-            sx={{ maxWidth: '100%' }}
-            mx="auto"
-            withIndicators
-            plugins={[autoplay.current]}
-            onMouseEnter={autoplay.current.stop}
-            onMouseLeave={autoplay.current.reset}
-            breakpoints={[{ maxWidth: 'sm', slideSize: '100%', slideGap: 2 }]}
-            slideGap={1}
-            align="start"
-            slidesToScroll={mobile ? 1 : 2}
-        >
-            {slides}
-        </Carousel>
+      <Paper shadow="md" p="xl" className={`${classes.card} `}>
+        <div>
+          <Text className={`${classes.category}`} size="xs">
+            <h1>this is slider</h1>
+          </Text>
+          <Title order={3} className={`${classes.title}`}>
+            {title}
+          </Title>
+          <Button variant="gradient" color="red">
+            Read article
+          </Button>
+        </div>
+        <div>
+          <Lottie animationData={image} loop={true} className="h-[550px]" />;
+        </div>
+      </Paper>
     );
+  }
+  const data = [
+    {
+      image: heroImg,
+      title: "Best forests to visit in North America",
+      category: "nature",
+    },
+    {
+      image: heroImg,
+      title: "Hawaii beaches review: better than you think",
+      category: "beach",
+    },
+    {
+      image: heroImg3,
+      title: "Mountains at night: 12 best locations to enjoy the view",
+      category: "nature",
+    },
+    {
+      image: heroImg,
+      title: "Aurora in Norway: when to visit for best experience",
+      category: "nature",
+    },
+    {
+      image: heroImg,
+      title: "Best places to visit this winter",
+      category: "tourism",
+    },
+    {
+      image: heroImg,
+      title: "Active volcanos reviews: travel at your own risk",
+      category: "nature",
+    },
+  ];
+
+  const theme = useMantineTheme();
+  const mobile = useMediaQuery(`(max-width: ${theme.breakpoints.sm}px)`);
+  const slides = data.map((item) => (
+    <Carousel.Slide key={item.title}>
+      <Card {...item} />
+    </Carousel.Slide>
+  ));
+
+  return (
+    <Carousel
+      sx={{ maxWidth: "100%" }}
+      mx="auto"
+      withIndicators
+      plugins={[autoplay.current]}
+      onMouseEnter={autoplay.current.stop}
+      onMouseLeave={autoplay.current.reset}
+      breakpoints={[{ maxWidth: "sm", slideSize: "100%", slideGap: 2 }]}
+      slideGap={1}
+      align="start"
+      slidesToScroll={mobile ? 1 : 2}
+    >
+      {slides}
+    </Carousel>
+  );
 };
 
 export default Hero;
