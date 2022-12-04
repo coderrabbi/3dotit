78c429f7044ad81d8fd959e006eda8206cf105f2
coderrabbie <golamrabbi.dv@gmail.com>
2022-12-04 21:08:24 +0600

    banner done

--- client/components/HomeScreen/HomeScreen.js
+++ client/components/HomeScreen/HomeScreen.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import Footer from '../Common/Footer/Footer';
-import { MainHeader } from '../Common/Navbar/Header';
-import Hero from '../Hero/Hero';
-import Layout from '../Layout/Layout';
+import React from "react";
+import Footer from "../Common/Footer/Footer";
+import { MainHeader } from "../Common/Navbar/Header";
+import { Services } from "../Common/Services/Services";
+import Hero from "../Hero/Hero";
+import Layout from "../Layout/Layout";
 
 const HomeScreen = () => {
-    return (
-        <>
-            <Layout>
-                <Hero />
-            </Layout>
-        </>
-    );
+  return (
+    <>
+      <Layout>
+        <Hero />
+        <Services />
+      </Layout>
+    </>
+  );
 };
 
 export default HomeScreen;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,13 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        './app/**/*.{js,ts,jsx,tsx}',
-        './pages/**/*.{js,ts,jsx,tsx}',
-        './components/**/*.{js,ts,jsx,tsx}',
-    ],
-    theme: {
-        extend: {},
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#00040f",
+        secondary: "#f97316",
+        dimWhite: "rgba(255, 255, 255, 0.7)",
+        dimBlue: "rgba(9, 151, 124, 0.1)",
+      },
     },
+  },
 
-    plugins: [require('tailwindcss-animate')],
+  plugins: [require("tailwindcss-animate")],
 };
